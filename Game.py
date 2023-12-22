3753a12c3a132ebdda302d6824b6bcac50770244
Brendan Michael Dougherty <144568526+vortech11@users.noreply.github.com>
2023-12-22 13:02:14 -0600

    Made game playable

--- Game.py
+++ Game.py
@@ -1,5 +1,6 @@
 import pygame
-import os
+import random
+import time
 
 height = 400
 length = 400
@@ -17,19 +18,20 @@ bottom_pipe = pygame.Rect(0, 0, 50, 300)
 top_pipe = pygame.Rect(0, 100, 50, 400)
 positions = []
 positions.append(400)
-positions.append(50)
+positions.append(random.randrange(-130, 30))
+start = time.time()
 
-run = True
-while run:
+game_over = False
+while not(game_over):
     clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                yspeed = 7.5
+                yspeed = 5
             
-    yspeed -= 0.25
+    yspeed -= 0.2
 
     if abs(yspeed) > 7.5:
         if yspeed > 0:
@@ -38,19 +40,26 @@ while run:
             yspeed = -7.5
 
     player.y -= yspeed
+
+    if  time.time() - start > 3:
+        positions.append(400)
+        positions.append(random.randrange(-130, 30))
+        start = time.time()
     
     window.fill(0)
     
     for x in range(0, len(positions), 2):
-        positions[x] -= 0.5
+        positions[x] -= 1.5
         bottom_pipe.x = positions[x]
         top_pipe.x = positions[x]
-        bottom_pipe.y = 0 - positions[x + 1]
-        top_pipe.y = 0 - positions[x + 1]
-        pygame.draw.rect(window, 999, bottom_pipe)
-        pygame.draw.rect(window, 999, top_pipe)
+        bottom_pipe.y = -235 - positions[x + 1]
+        top_pipe.y = 235 - positions[x + 1]
+        if bottom_pipe.colliderect(player) or top_pipe.colliderect(player):
+            game_over = True
+        pygame.draw.rect(window, (19, 156, 9), bottom_pipe)
+        pygame.draw.rect(window, (19, 156, 9), top_pipe)
 
-    pygame.draw.rect(window, 999, player)
+    pygame.draw.rect(window, (222, 200, 0), player)
     pygame.display.flip()
 
 pygame.quit()
